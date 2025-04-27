017f1a0f7a7c696ccee6b74b6f9b120e94216b94
Jörn Stöhler <joern@stoehler.eu>
2025-04-27 02:45:36 +0200

    fix: disable ssr for gh pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,6 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // Disable SSR for static SPA deployment on GitHub Pages
+  ssr: false,
 } satisfies Config;
